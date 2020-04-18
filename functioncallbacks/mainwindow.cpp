86c81112dc63752bf2df453e03948045a38dcf8a
szkmiyabi <led.centipede@gmail.com>
2020-04-18 23:07:14 +0900

    Function callbacks sample追加

---
+++ functioncallbacks/mainwindow.cpp
@@ -0,0 +1,53 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    //マウスのキャプチャ設定
+    this->setMouseTracking(true);
+    ui->centralwidget->setMouseTracking(true);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+//キーボードイベント処理の実装
+void MainWindow::resizeEvent(QResizeEvent *event)
+{
+    qDebug()<<"Old size:"<<event->oldSize()<<",New size:"<<event->size();
+}
+
+void MainWindow::keyPressEvent(QKeyEvent *event)
+{
+    if(event->key() == Qt::Key_Escape) {
+        this->close();
+    }
+    qDebug()<<event->text()<<"has been pressed";
+}
+
+void MainWindow::keyReleaseEvent(QKeyEvent *event)
+{
+    qDebug()<<event->text()<<"has been pressed";
+}
+
+//マウスイベント処理の実装
+void MainWindow::mouseMoveEvent(QMouseEvent *event)
+{
+    qDebug()<<"Position:"<<event->pos();
+}
+
+void MainWindow::mousePressEvent(QMouseEvent *event)
+{
+    qDebug()<<"Mouse pressed:"<<event->button();
+}
+
+void MainWindow::mouseReleaseEvent(QMouseEvent *event)
+{
+    qDebug()<<"Mouse released:"<<event->button();
+}
---
+++ functioncallbacks/mainwindow.h
@@ -0,0 +1,33 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QDebug>
+#include <QResizeEvent>
+#include <QKeyEvent>
+#include <QMouseEvent>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+    void resizeEvent(QResizeEvent *event);
+    void keyPressEvent(QKeyEvent *event);
+    void keyReleaseEvent(QKeyEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+    void mousePressEvent(QMouseEvent *event);
+    void mouseReleaseEvent(QMouseEvent *event);
+
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
