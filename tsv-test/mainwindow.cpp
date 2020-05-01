e197886d1a4835e1e4303d3e73ff8ed26551cd6e
szkmiyabi <led.centipede@gmail.com>
2020-05-02 03:54:08 +0900

    TSVファイルの処理テストプロジェクト追加

---
+++ tsv-test/mainwindow.cpp
@@ -0,0 +1,76 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QFile>
+#include <QFileDialog>
+#include <QDebug>
+#include <QTextStream>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+//pushButtonクリック
+void MainWindow::on_pushButton_clicked()
+{
+    read_tsv_file();
+}
+
+//テキストファイルのパス取得
+QString MainWindow::get_file_path()
+{
+    QString path = QCoreApplication::applicationDirPath();
+    QString filepath = QFileDialog::getOpenFileName(
+                        this,
+                        tr(u8"ファイルを選択してください"), //u8はMSVC文字化け対策
+                        path,
+                        tr(u8"tsv file(*.txt)")
+    );
+    return  filepath;
+}
+
+//TSVファイルの処理
+void MainWindow::read_tsv_file()
+{
+    //結果格納ベクタをリセット
+    url_arr.clear();
+    //ファイルパス取得
+    QString filename = get_file_path();
+    if(filename.size() == 0) return;
+
+    //ファイルオブジェクト取得
+    QFile fp(filename);
+    if(!fp.open(QIODevice::ReadOnly)) return;
+
+    //テキストストリームを取得
+    QTextStream sr(&fp);
+
+    //最終行まで1行づつ読み込む
+    while(!sr.atEnd()) {
+        QString line = sr.readLine(0);
+        QList<QString> tmp = line.split("\t");
+        QVector<QString> row;
+        row.append(tmp[0]);
+        row.append(tmp[1]);
+        url_arr.append(row);
+    }
+
+    //ファイルオブジェクト破棄
+    fp.close();
+
+    //Debug出力
+    foreach(const QVector<QString>& row, url_arr){
+        QString pid = row.at(0);
+        QString url = row.at(1);
+        qDebug() <<"pid: " <<pid <<", url: " <<url;
+    }
+
+}
---
+++ tsv-test/mainwindow.h
@@ -0,0 +1,30 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_pushButton_clicked();     //pushButtonクリック
+
+private:
+    Ui::MainWindow *ui;
+
+    QVector<QVector<QString>> url_arr;   //結果格納配列
+
+    QString get_file_path();           //ファイルパス取得
+    void read_tsv_file();              //TSVファイルの処理
+
+};
+#endif // MAINWINDOW_H
